65a37d0e7d9d26c0fa2ea1c92ecf83c34bbc9cd7
joypicar <joysancute@gmail.com>
2023-02-19 23:24:05 +0800

    added profiles

--- src/main/java/com/joyspring/demo/DemoApplication.java
+++ src/main/java/com/joyspring/demo/DemoApplication.java
@@ -2,10 +2,12 @@ package com.joyspring.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Profile({"dev", "prod"})
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
